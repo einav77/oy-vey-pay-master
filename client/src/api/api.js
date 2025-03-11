d3464d0be6b16788fad3080f7c66a9561705e042
einav77 <einavelkes@gmail.com>
2025-03-11 11:29:21 +0200

    gggg

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://oy-vey-pay-master.onrender.com/api' : 'http://localhost:3000/api'
+const baseURL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3000/api'
 
 export default axios.create({
     baseURL,
